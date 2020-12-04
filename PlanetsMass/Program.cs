b030820ffffdc425b1046177ce2a51151dd90223
Opilane TTHK <opilane@tthk.ee>
2020-12-04 10:00:06 +0200

    Create Web Page

---
+++ PlanetsMass/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PlanetsMass
+{
+    class Program
+    {
+        public class Item
+        {
+            string name;
+            int price;
+
+            public Item(string _name, int _price)
+            {
+                name = _name;
+                price = _price;
+            }
+
+            public string Name { get { return name; } }
+
+            public int Price { get { return price; } }
+        }
+        static void Main(string[] args)
+        {
+            ReadFromShoppingList();
+        }
+
+        public static void ReadFromShoppingList()
+        {
+            string filePath = @"C:\Users\opilane\Planets";
+            string fileName = @"SolarSystem.txt";
+            List<Item> shoppingItems = new List<Item>();
+
+            List<string> linesFromFile = File.ReadAllLines(Path.Combine(filePath, fileName)).ToList();
+            int total = 0;
+            foreach (string line in linesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                Item newItem = new Item(tempArray[0], Int32.Parse(tempArray[1]));
+                shoppingItems.Add(newItem);
+            }
+            foreach (Item item in shoppingItems)
+            {
+                    Console.WriteLine($"Item: {item.Name}; Price: {item.Price}");
+                    total += item.Price;
+            }
+            Console.WriteLine($"total {total}");
+        }
+    }
+}
---
+++ SolarSystem/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SolarSystem
+{
+    class Program
+    {
+        public class Item
+        {
+            string name;
+            int price;
+
+            public Item(string _name, int _price)
+            {
+                name = _name;
+                price = _price;
+            }
+
+            public string Name { get { return name; } }
+
+            public int Price { get { return price; } }
+        }
+        static void Main(string[] args)
+        {
+            WriteShoppingList();
+        }
+
+        public static void WriteShoppingList()
+        {
+            List<string> shoppingList = new List<string>();
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine("Enter Planet:");
+
+                string item = Console.ReadLine();
+                Console.WriteLine("Enter mass of planets:");
+                string price = Console.ReadLine();
+
+                string line = $"{item}/{price}";
+                shoppingList.Add(line); // add a line to the shoppingList list
+            }
+            string filePath = @"C:\Users\opilane\Planets";
+            string fileName = @"SolarSystem.txt";
+            File.WriteAllLines(Path.Combine(filePath, fileName), shoppingList);
+        }
+    }
+}
